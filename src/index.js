16fa1361faeb4dc49ac25590172d4ef103e4375e
yijiyap <yijiyap@gmail.com>
2023-08-21 23:59:26 +0800

    Created pages and link with firebase

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBLS_RdY5eC8wTA87g4J2WPBQRKvtOAQQU",
+    authDomain: "agent-yj-ade4a.firebaseapp.com",
+    projectId: "agent-yj-ade4a",
+    storageBucket: "agent-yj-ade4a.appspot.com",
+    messagingSenderId: "720515191410",
+    appId: "1:720515191410:web:a09e7d79676e6fd4cb68eb"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
